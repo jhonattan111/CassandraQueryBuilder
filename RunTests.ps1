03d03e1300e6fabe57de4935a4aab259543aeac9
Jhonattan <jhonattan10alves@gmail.com>
2025-03-02 21:45:37 -0300

    master adding ps1

---
+++ RunTests.ps1
@@ -0,0 +1,22 @@
+﻿dotnet test --collect:"XPlat Code Coverage"
+
+$projectName = "CassandraQueryBuilder.Tests"
+
+$coverageDir = Get-ChildItem -Directory ".\$projectName\TestResults" | Sort-Object LastWriteTime | Select-Object -Last 1
+
+$guid = $coverageDir.Name
+
+$reportPath = "coveragereport\$guid\index.html"
+reportgenerator -reports:".\$projectName\TestResults\$guid\coverage.cobertura.xml" -targetdir:"coveragereport\$guid" -reporttypes:Html
+
+$fullPath = (Join-Path -Path (Get-Location) -ChildPath $reportPath)
+$fileUrl = $fullPath -replace '\\', '/'
+$fileUrl = "file:///" + $fileUrl
+
+Write-Host "Relat�rio gerado em: coveragereport\$guid"
+
+Start-Process $fileUrl
+
+
+Write-Host "Aperte qualquer bot�o para fechar..."
+[System.Console]::ReadKey() | Out-Null
